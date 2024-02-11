272e663f5753908d59f07ab89eb2bf66d0b066bd
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-11 13:52:36 +0530

    [ADDED] : YOGESH Program for Show Msg on Window Using Option Package with Variable

---
+++ OptPakWITHvar.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+class OptPakWITHvar
+{
+	public static void main(String[] args)
+	{
+		String msg="Hello sir ,\nI am Yogesh Badgujar ,Nice to meet you" ;
+
+		JOptionPane.showMessageDialog(null , msg);
+	}
+}
