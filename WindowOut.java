832b83835ecb74e99c38a6d8461524b8e5168fb0
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-11 13:35:29 +0530

    [ADDED] : YOGESH Window Output Program

---
+++ WindowOut.java
@@ -0,0 +1,13 @@
+package com.yog.OptionalPackage;
+
+import javax.swing.JOptionPane;
+
+public class WindowOut {
+	
+	public static void main(String[] args)
+	{
+		JOptionPane.showMessageDialog(null ,"My First Optional Package Program");
+		
+		System.exit(0);
+	}
+}
