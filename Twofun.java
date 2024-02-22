9dbb92a2e84429f74faf6b8ac718fef2af206afa
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-22 19:34:24 +0530

    [ADDED] : YOGESH Two function in jack_pack and Twofun class

---
+++ Twofun.java
@@ -0,0 +1,15 @@
+package jack;
+
+public class Twofun
+{
+	public void msg1()
+	{
+		System.out.println("First Function call");
+	}
+
+	public void msg2()
+	{
+		System.out.println("Second Function call");
+	}
+
+}
