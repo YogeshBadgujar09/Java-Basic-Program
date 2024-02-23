106f8ea18c0a7868596f65f3cb443d28c8dcc84e
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-23 15:30:47 +0530

    [ADDED] : YOGESH String length()

---
+++ StringLengthFun.java
@@ -0,0 +1,14 @@
+package com.yogesh.stringfunction;
+
+public class StringLengthFun {
+	
+	public static void main(String args[])
+	{
+		String var="Infogalaxy";
+		
+		System.out.println(var);
+		System.out.println(var.length()); // length() use 
+		
+	}
+
+}
