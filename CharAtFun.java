84e6fb1d1c2c04084d1e103fc9656c82988a269a
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-23 15:27:11 +0530

    [ADDED] : YOGESH String charAt()

---
+++ CharAtFun.java
@@ -0,0 +1,18 @@
+package com.yogesh.stringfunction;
+
+public class CharAtFun {
+
+	public static void main(String[] args) {
+		
+		String a = "yogesh";
+		System.out.println(a.charAt(2));
+		
+		String b= "Rohit";
+		System.out.println(b.charAt(0));
+		System.out.println(b.charAt(1));
+		System.out.println(b.charAt(2));
+		System.out.println(b.charAt(3));
+		System.out.println(b.charAt(4));
+	}
+
+}
