c0c3d76d751421e27d7797d8d85211e3ee514c54
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-23 15:31:51 +0530

    [ADDED] : YOGESH String trim()

---
+++ TrimFun.java
@@ -0,0 +1,18 @@
+package com.yogesh.stringfunction;
+
+public class TrimFun {
+	
+	public static void main(String args[])
+	{
+		String var = "         Yogesh Bagujar      ";
+		
+		System.out.println(var);
+		
+		System.out.println(var.trim());
+		
+		
+		 
+		
+	}
+
+}
