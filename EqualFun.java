8f4ed94214d7fc620c051f14d0ac92a9a4ed35e5
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-24 23:52:43 +0530

    [ADDED] : Yogesh Use equals() fun

---
+++ EqualFun.java
@@ -0,0 +1,30 @@
+package com.yogesh.stringfunction;
+
+public class EqualFun {
+	
+	public static void main(String args[])
+	{
+		String a="Welcome";
+		String b="Java";
+		
+		System.out.println(a.equals(b));
+		
+		String c="Yogesh";
+		String d="Suresh";
+		
+		System.out.println(c.equals(d));
+		
+		String e="Ramesh";
+		String f="ramesh";
+		
+		System.out.println(e.equals(f));
+		
+		String g="Ramesh";
+		String h="Ramesh";
+		
+		System.out.println(g.equals(h));
+		
+		
+		}
+
+}
