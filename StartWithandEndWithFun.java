16ed45a9f873750db7c8d8af71b93a7c86a54a2f
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-24 23:54:15 +0530

    [ADDED] : Yogesh Use of startsWith() and endsWith()

---
+++ StartWithandEndWithFun.java
@@ -0,0 +1,14 @@
+package com.yogesh.stringfunction;
+
+public class StartWithandEndWithFun {
+
+	public static void main(String[] args) {
+		 
+		String var = "Yogesh";
+		
+	System.out.println(var.startsWith("Yo"));
+	System.out.println(var.endsWith("H"));
+
+	}
+
+}
