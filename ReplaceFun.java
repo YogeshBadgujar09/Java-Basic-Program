f79a2a0be3cab89db578e0b7da15a53d2f1771ec
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-24 23:55:25 +0530

    [ADDED] : Yogesh use replace() fun

---
+++ ReplaceFun.java
@@ -0,0 +1,12 @@
+package com.yogesh.stringfunction;
+
+public class ReplaceFun {
+
+	public static void main(String[] args) {
+		
+		String var = "Sachin";
+		
+		System.out.println(var.replace("Sa","Ra"));
+	}
+
+}
