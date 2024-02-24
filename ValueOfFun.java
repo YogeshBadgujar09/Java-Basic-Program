48bd427a4f8cfe00b2b97f40221f8aedb4ce817b
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-24 23:57:09 +0530

    [ADDED] : Yogesh use of valueOf()

---
+++ ValueOfFun.java
@@ -0,0 +1,13 @@
+package com.yogesh.stringfunction;
+
+public class ValueOfFun {
+
+	public static void main(String[] args) {
+		 
+		int a=10;
+		String s = String.valueOf(a);
+		
+		System.out.println(s+a);
+	}
+
+}
