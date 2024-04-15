77739a89dca1d3a10ea32a2647fa2ec8afa6076f
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-04-15 23:41:50 +0530

    [ADDED] : Yogesh - subString function in Java

---
+++ SubString.java
@@ -0,0 +1,14 @@
+package com.yogesh.stringfunction;
+
+public class SubString {
+    public static void main(String[] args) {
+
+        String name = "InfoGalaxy";
+
+        System.out.println(name.substring(4,6));
+        System.out.println(name.substring(5));
+        System.out.println(name.subSequence(3,7));
+
+
+    }
+}
