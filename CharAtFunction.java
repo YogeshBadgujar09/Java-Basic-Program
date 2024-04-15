31a061ebdc9ef79438c65b423bf55a3f1962ef82
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-04-15 23:40:23 +0530

    [ADDED] : Yogesh - charAt function in java

---
+++ CharAtFunction.java
@@ -0,0 +1,11 @@
+package com.yogesh.stringfunction;
+
+public class CharAtFunction {
+
+    public static void main(String[] args) {
+
+        String name = "Yogesh";
+
+        System.out.println(name.charAt(4));
+    }
+}
