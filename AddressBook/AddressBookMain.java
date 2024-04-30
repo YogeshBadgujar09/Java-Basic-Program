88892019a87ede8a6dfe1bdc73a39470349b7d6e
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-04-30 15:27:36 +0530

    [MODIFIED] : Yogesh - edit Contacts using Name

--- AddressBook/AddressBookMain.java
+++ AddressBook/AddressBookMain.java
@@ -3,43 +3,102 @@ import java.util.Scanner;
 class AddressBookMain
 {
 
-	ContactsModul contactsModul = new ContactsModul();
+	ContactsModel contactsModel = new ContactsModel();
+	Scanner sc = new Scanner(System.in);
 
-	AddressBookMain()
-	{
-		Scanner sc = new Scanner(System.in);
 		
+	public void addContacts()
+	{	
 		System.out.print("Enter First Name :");
-		contactsModul.set_first_Name(sc.next());
+		contactsModel.set_first_Name(sc.next());
 	
 		System.out.print("Enter Last Name :");
-		contactsModul.set_last_Name(sc.next());
+		contactsModel.set_last_Name(sc.next());
 
 		System.out.print("Enter Address :");
-		contactsModul.set_address(sc.next());
+		contactsModel.set_address(sc.next());
 
 		System.out.print("Enter city :");
-		contactsModul.set_city(sc.next());
+		contactsModel.set_city(sc.next());
 
 		System.out.print("Enter State :");
-		contactsModul.set_state(sc.next());
+		contactsModel.set_state(sc.next());
 
 		System.out.print("Enter Zip Code :");
-		contactsModul.set_zip_Code(sc.next());
+		contactsModel.set_zip_Code(sc.next());
 
 		System.out.print("Enter Phone Number :");
-		contactsModul.set_phone_Number(sc.next());
+		contactsModel.set_phone_Number(sc.next());
 		
 		System.out.print("Enter Email :");
-		contactsModul.set_email_Id(sc.next());
+		contactsModel.set_email_Id(sc.next());
+	
+	}
+
+	public void showContacts()
+	{
+		System.out.println("\nFirst Name :" + contactsModel.get_first_Name());
+		System.out.println("Last Name :" + contactsModel.get_last_Name());
+		System.out.println("Address :" + contactsModel.get_address());
+		System.out.println("City :" + contactsModel.get_city());
+		System.out.println("State :" + contactsModel.get_state());
+		System.out.println("Zip Code :" + contactsModel.get_zip_Code());
+		System.out.println("Phone NUmber :" + contactsModel.get_phone_Number());
+		System.out.println("Gmail :" + contactsModel.get_email_Id());
 
 	}
 
+	public void editContacts()
+	{
+
+		System.out.println("\nEnter First Name to Edit Contact :");
+		String fname = sc.next();
+
+		if(fname.equals(contactsModel.get_first_Name()))
+		{
+			System.out.println("Contact Found.");
+			
+			System.out.print("Enter Last Name :");
+			contactsModel.set_last_Name(sc.next());
+
+			System.out.print("Enter Address :");
+			contactsModel.set_address(sc.next());
+	
+			System.out.print("Enter city :");
+			contactsModel.set_city(sc.next());
+	
+			System.out.print("Enter State :");
+			contactsModel.set_state(sc.next());
+	
+			System.out.print("Enter Zip Code :");
+			contactsModel.set_zip_Code(sc.next());
+		
+			System.out.print("Enter Phone Number :");
+			contactsModel.set_phone_Number(sc.next());
+			
+			System.out.print("Enter Email :");
+			contactsModel.set_email_Id(sc.next());
+		}
+		else
+		{
+			System.out.println("Contact not found");
+		}
+
+	}
+		
+	
+	 	
+
 	public static void main(String args[])
 	{
-		System.out.println("Welcome To Address Book");
+		System.out.println("Welcome To Address Book\n");
 		
 		AddressBookMain addressBookMain = new AddressBookMain();
+		addressBookMain.addContacts();
+		addressBookMain.showContacts();
+		addressBookMain.editContacts();
+		addressBookMain.showContacts();
+	 
 	}
 
 }
