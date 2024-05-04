bf68ef0a6210339551ec854049f384ea1b8a3e45
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-05-04 23:54:49 +0530

    [ADDED] : Yogesh- MOdel class Use for data abstraction with setter getter method

---
+++ ModelClass.java
@@ -0,0 +1,37 @@
+class ModelClass{
+
+	String firstName;
+	String lastName ;
+	String number ;
+
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+	public void setNumber(String number)
+	{
+		this.number = number ;
+	}
+
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	public String getLastName()
+	{
+		return lastName;
+	}
+	public String getNumber()
+	{
+		return number;
+	}
+
+	
+
+	
+	
+}
