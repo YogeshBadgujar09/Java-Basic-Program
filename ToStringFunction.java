f7dded79682c4199012be9efa9d1745ea5736427
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-05-04 23:59:17 +0530

    [ADDED] : Yogesh - ToString function return objects string Properties

---
+++ ToStringFunction.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class ToStringFunction
+{
+	ModelClass modelClass = new ModelClass();
+	Scanner sc = new Scanner(System.in);
+
+
+
+
+	public static void main(String args[])
+	{
+
+		
+	}
+
+
+
+
+}
