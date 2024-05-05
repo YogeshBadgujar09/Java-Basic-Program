a286d6596ca1dfd827baab4a75aee22203289ddc
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-05-05 12:49:14 +0530

    [ADDED] : Yogesh ToString Function return Objects Properties Value

--- ToStringFunction.java
+++ ToStringFunction.java
@@ -5,13 +5,30 @@ class ToStringFunction
 	ModelClass modelClass = new ModelClass();
 	Scanner sc = new Scanner(System.in);
 
+	public void setInfo()
+	{
+		System.out.println("Enter First Name");
+		modelClass.setFirstName(sc.next());
 
+		System.out.println("Enter Last Name");
+		modelClass.setLastName(sc.next());
+
+		System.out.println("Enter Number");
+		modelClass.setNumber(sc.next());
+	}
+
+	public void getInfo()
+	{
+			System.out.println("\n" + modelClass.toString());
+	}
 
 
 	public static void main(String args[])
 	{
+		ToStringFunction toStringFunction = new ToStringFunction();
+		toStringFunction.setInfo();
+		toStringFunction.getInfo();
 
-		
 	}
 
 
