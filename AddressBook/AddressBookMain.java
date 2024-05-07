0abaae77fb49736ed830f55282a262b8de5cd967
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-05-07 20:39:08 +0530

    [MODIFIED] : Yogesh - Add Multiple Contact

--- AddressBook/AddressBookMain.java
+++ AddressBook/AddressBookMain.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class AddressBookMain
 {
 
-	ContactsModel contactsModel = new ContactsModel();
 	Scanner sc = new Scanner(System.in);
 
-	
+	ArrayList<ContactsModel> contactsList = new ArrayList<ContactsModel>();
+
 	public void contactsOperation()
 	{
-		while(true)
-		{
 
-			int choice ;
-		
-			System.out.println("\n1.Add Contact \n2.Edit Contact \n3.Show Contact \n4.Delete Contact \n5.Exit");
+		int choice ;
+
+		do{
+
+			System.out.println("\n1.Add Contact \n2.Edit Contact \n3.Show Contact \n4.Delete Contact \n5.FindContact\n6.EXIT");
 			System.out.println("\nEnter Choice :");
 			choice = sc.nextInt();
 
@@ -23,41 +24,43 @@ class AddressBookMain
 				case 1:
 				addContacts();
 				break ;
-	
-				case 2:
+
+			/*	case 2:
 				editContacts();
-				break ;
+				break ; */
 
 				case 3:
 				showContacts();
 				break ;
 
+			/*
 				case 4:
 				deleteContacts();
 				break ;
-		
+
+			*/
+
+				case 5:
+				findContact();
+				break;
+
 				default :
 				System.out.println("Case not Match");
-			}	
-			
-			if(choice == 5)
-			{
-				System.out.println("\nEXIT");
-				break;
 			}
-		 }
-		
 
+ 		 }while(choice <= 5);
 
 	}
 
-	 
-		
+
 	public void addContacts()
-	{	
+	{
+
+		ContactsModel contactsModel = new ContactsModel();
+
 		System.out.print("Enter First Name :");
 		contactsModel.set_first_Name(sc.next());
-	
+
 		System.out.print("Enter Last Name :");
 		contactsModel.set_last_Name(sc.next());
 
@@ -75,24 +78,28 @@ class AddressBookMain
 
 		System.out.print("Enter Phone Number :");
 		contactsModel.set_phone_Number(sc.next());
-		
+
 		System.out.print("Enter Email :");
 		contactsModel.set_email_Id(sc.next());
-	
+
+		contactsList.add(contactsModel);
+
 	}
+
 
 	public void showContacts()
 	{
-		if(contactsModel != null)
-		{
-			System.out.println("\n" + contactsModel.toString()); 
+		for(int i=0 ; i<contactsList.size() ; i++){
+
+			ContactsModel contactsModel = contactsList.get(i);
+
+			System.out.println(contactsModel.toString());
+
 		}
-		else
-		{
-			System.out.println("Contact is Not Found.");
-		}
-		
+
 	}
+
+	/*
 
 	public void editContacts()
 	{
@@ -103,25 +110,25 @@ class AddressBookMain
 		if(fname.equals(contactsModel.get_first_Name()))
 		{
 			System.out.println("Contact Found.");
-			
+
 			System.out.print("Enter Last Name :");
 			contactsModel.set_last_Name(sc.next());
 
 			System.out.print("Enter Address :");
 			contactsModel.set_address(sc.next());
-	
+
 			System.out.print("Enter city :");
 			contactsModel.set_city(sc.next());
-	
+
 			System.out.print("Enter State :");
 			contactsModel.set_state(sc.next());
-	
+
 			System.out.print("Enter Zip Code :");
 			contactsModel.set_zip_Code(sc.next());
-		
+
 			System.out.print("Enter Phone Number :");
 			contactsModel.set_phone_Number(sc.next());
-			
+
 			System.out.print("Enter Email :");
 			contactsModel.set_email_Id(sc.next());
 		}
@@ -131,13 +138,13 @@ class AddressBookMain
 		}
 
 	}
-		
+
 	public void deleteContacts()
-	{		
+	{
 		System.out.println("Enter First Name to Delete Contact :");
 		String fname = sc.next();
 
-		if(fname.equals(contactsModel.get_first_Name()));
+		if(fname.equals(contactsModel.get_first_Name()))
 		{
 			System.out.println("Contact is Delete SuccessFully");
 			contactsModel = null ;
@@ -146,19 +153,46 @@ class AddressBookMain
 		{
 			System.out.println("Contact can't delete/Found.");
 		}
-	
+
 	}
-	
-	 	
+
+	*/
+
+	public void findContact()
+	{
+		String fname ;
+		System.out.println("Enter First Name to Find Contact :");
+		fname = sc.next();
+
+				for(int i=0 ; i<contactsList.size() ; i++)
+				{
+
+					ContactsModel contactsModel = contactsList.get(i);
+
+						if(fname.equals(contactsModel.get_first_Name()))
+						{
+							System.out.println("Contact Found");
+							break  ;
+						}
+						else
+						{
+							if()
+						}
+						/*if(fname.equals(contactsModel.get_first_Name()) == false)
+			     		{
+						 		System.out.println("Contact Not Found");
+						}*/
+				}
+
+	}
 
 	public static void main(String args[])
 	{
 		System.out.println("Welcome To Address Book\n");
-		
-		AddressBookMain addressBookMain = new AddressBookMain();
-		addressBookMain.contactsOperation();
-		
-	 
+
+		AddressBookMain addressBookMain1 = new AddressBookMain();
+		addressBookMain1.contactsOperation();
+
 	}
 
 }
