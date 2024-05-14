0794988b75ba79a2ca1c6e122be3021ace3b3a52
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-05-14 21:05:07 +0530

    [ADDED] : Yogesh - another Logic for Program

---
+++ Java-LeetCode/TwoIndexSum2.java
@@ -0,0 +1,26 @@
+class TwoIndexSum2
+{
+
+	public static void main(String args[])
+	{
+		int[] arr = {2,4,5,8};
+		int target = 7 ;
+
+		for(int i=0 ; i<arr.length ; i++)
+		{
+			for(int j=1 ; j<arr.length ; j++)
+			{
+
+				if(arr[j] == target - arr[i])
+				{
+					System.out.println("values of Indexes :\n" + arr[i] + " + " + arr[j] + " == " + target);
+					System.out.println("Indexex of arr[] : " + i + " & " + j );
+				}
+			}
+
+		}
+
+	}
+
+
+}
