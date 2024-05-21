b39a209580e92c23516bec9b2619900d94f527f1
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-05-21 23:08:30 +0530

    [ADDED] : Yogesh - Wrapper class Program

---
+++ WrapperClassDemo1.java
@@ -0,0 +1,17 @@
+class WrapperClassDemo{
+
+	public static void main(String args[])
+	{
+		Integer a = 10 ;
+		Integer b = 20 ;
+
+		Integer c = 0 ;
+
+		c = a + b;
+		System.out.println("Add :" + c);
+
+
+	}
+
+
+}
