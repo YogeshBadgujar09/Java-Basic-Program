fc6f67403ed6726f7661f6f6c7934b5248d9a8c0
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-06-13 21:14:32 +0530

    [ADDED] : Yogesh - try to create own class

---
+++ CreateOwnStringClass/OwnStringClass.java
@@ -0,0 +1,16 @@
+public class OwnStringClass{
+
+	public void StringLength(String string)
+	{
+		int i = 0 ;
+
+		char[] str ;
+
+		for(i=0 ; i<100 ; i++)
+		{
+			str[i] = Character.parse(string);
+		}
+
+	}
+
+}
---
+++ CreateOwnStringClass/OwnStringClassFunctionUse.java
@@ -0,0 +1,14 @@
+class OwnStringClassFunctionUse{
+
+
+	public static void main(String args[])
+	{
+		OwnStringClass obj =  new OwnStringClass();
+
+		String name = "yogesh"	;
+
+
+		System.out.println("Length of String : " + obj.StringLength(name));
+	}
+
+}
