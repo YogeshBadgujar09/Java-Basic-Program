b3053465d574ea76923fd4914ccd8f1ce2c68209
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-06-19 20:49:36 +0530

    [ADDED] : Yogesh - CharAtArray function use

---
+++ CharAtArray.java
@@ -0,0 +1,15 @@
+package com.yogesh.stringfunction;
+
+public class CharAtArray {
+
+    public static void main(String[] args) {
+
+        String name = "Nayan" ;
+
+         for(char x : name.toCharArray())
+         {
+             System.out.println(x);
+         }
+
+    }
+}
