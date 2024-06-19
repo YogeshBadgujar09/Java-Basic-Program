62a6e81abd231a4c66dd95d11a1c79c4823e68fd
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-06-19 20:50:33 +0530

    [ADDED] : Yogesh - CharAt function use

---
+++ CharAtUse.java
@@ -0,0 +1,19 @@
+package com.yogesh.stringfunction;
+
+public class CharAtUse {
+
+    public static void main(String[] args) {
+
+        String name = "yogesh";
+
+        int m = 0 ;
+        for (int i=0 ; i<name.length();i++)
+        {
+            char x = name.charAt(i);
+
+            System.out.println(x);
+
+
+        }
+    }
+}
