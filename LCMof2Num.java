7aa7d4b93c74584eaa54df35a5b234cd9329e09a
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-06-27 23:42:04 +0530

    [ADDED] : Yogesh - LCM program find

---
+++ LCMof2Num.java
@@ -0,0 +1,28 @@
+// Java Program to find
+// the LCM of two numbers
+
+
+class LCMof2Num{
+
+
+    public static void main(String[] args)
+    {
+
+        int a = 15, b = 25;
+
+        // Checking for the largest
+        // Number between them
+        int ans = (a > b) ? a : b;
+
+        // Checking for a smallest number that
+        // can de divided by both numbers
+        while (true) {
+            if (ans % a == 0 && ans % b == 0)
+                break;
+            ans++;
+        }
+
+        // Printing the Result
+        System.out.println("LCM of " + a + " and " + b  + " : " + ans);
+    }
+}
