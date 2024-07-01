994eba092a72bbac93e1d604f5147910bda072cb
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-07-01 23:32:53 +0530

    [MODIFIED] : Yogesh - Open Account Functionality, AccountInfo and Close Account Functionality

--- BankMangementSystem/src/com/yogesh/bankmanagement/BankManagementMain.java
+++ BankMangementSystem/src/com/yogesh/bankmanagement/BankManagementMain.java
@@ -1,11 +1,59 @@
 package com.yogesh.bankmanagement;
 
+import java.util.Scanner ;
+
 public class BankManagementMain {
+
+    Scanner scanner = new Scanner(System.in);
+    NewAccountInfoModel newAccountInfoModel = new NewAccountInfoModel();
+
+    public void openAccount() {
+        System.out.println("Enter Name :");
+        newAccountInfoModel.setName(scanner.next());
+
+        System.out.println("Enter aadhar number :");
+        newAccountInfoModel.setAadharNo(scanner.next());
+
+        System.out.println("Enter Mobile Number :");
+        newAccountInfoModel.setMobileNo(scanner.next());
+
+        System.out.println("Enter Your age :");
+        newAccountInfoModel.setAge(scanner.next());
+
+        System.out.println("Enter address");
+        newAccountInfoModel.setAddress(scanner.next());
+
+        System.out.println("Enter City :");
+        newAccountInfoModel.setCity(scanner.next());
+
+        System.out.println("Enter State :");
+        newAccountInfoModel.setState(scanner.next());
+
+        System.out.println("Enter email :");
+        newAccountInfoModel.setEmail(scanner.next());
+
+        System.out.println("Enter Initial Balance to Open Account :");
+        newAccountInfoModel.setBalance(scanner.next());
+    }
+
+    public void accountInfo() {
+        System.out.println(newAccountInfoModel.toString());
+    }
+
+    public void closeAccount() {
+        newAccountInfoModel = null;
+    }
+
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Bank Management System ");
 
+        BankManagementMain bankManagementMain = new BankManagementMain();
+        bankManagementMain.openAccount();
+        bankManagementMain.accountInfo();
+        bankManagementMain.closeAccount();
+      
     }
 
 }
--- BankMangementSystem/src/com/yogesh/bankmanagement/NewAccountInfoModel.java
+++ BankMangementSystem/src/com/yogesh/bankmanagement/NewAccountInfoModel.java
@@ -5,6 +5,7 @@ public class NewAccountInfoModel
 
     String name ;
     String aadharNo ;
+    String mobileNo ;
     String age ;
     String address ;
     String city ;
@@ -12,4 +13,90 @@ public class NewAccountInfoModel
     String email ;
     String balance ;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAadharNo() {
+        return aadharNo;
+    }
+
+    public void setAadharNo(String aadharNo) {
+        this.aadharNo = aadharNo;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "NewAccountInfoModel{" +
+                "name='" + name + '\'' +
+                ", aadharNo='" + aadharNo + '\'' +
+                ", mobileNo='" + mobileNo + '\'' +
+                ", age='" + age + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", email='" + email + '\'' +
+                ", balance='" + balance + '\'' +
+                '}';
+    }
 }
