2bc1a98b5202251a306791c085b4f1f17f3e80f3
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-07-01 22:41:53 +0530

    [ADDED] : Yogesh - Start New Project

---
+++ BankMangementSystem/src/com/yogesh/bankmanagement/BankManagementMain.java
@@ -0,0 +1,11 @@
+package com.yogesh.bankmanagement;
+
+public class BankManagementMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Bank Management System ");
+
+    }
+
+}
