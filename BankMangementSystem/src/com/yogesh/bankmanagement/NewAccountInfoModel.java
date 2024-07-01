890c32b6a6e36158036e117bf857c1b0023013fc
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-07-01 22:56:30 +0530

    [ADDED] : Yogesh - Create Model Class to Open an New Account Information

---
+++ BankMangementSystem/src/com/yogesh/bankmanagement/NewAccountInfoModel.java
@@ -0,0 +1,15 @@
+package com.yogesh.bankmanagement;
+
+public class NewAccountInfoModel
+{
+
+    String name ;
+    String aadharNo ;
+    String age ;
+    String address ;
+    String city ;
+    String state ;
+    String email ;
+    String balance ;
+
+}
