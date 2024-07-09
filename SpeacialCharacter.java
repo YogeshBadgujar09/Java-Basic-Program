f23097a5c088f9cced6fdf7d57d74d024646b9a1
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-07-09 20:45:33 +0530

    [ADDED] : Yogesh - Demo

---
+++ SpeacialCharacter.java
@@ -0,0 +1,15 @@
+class SpeacialCharacter {
+
+
+      public static void main (String[] args) {
+
+    String txt1 = "We are the so-called \"Vikings\" from the north.";
+	String txt2 = "It\'s alright.";
+	String txt3 = "The character \\ is called backslash.";
+
+	System.out.println(txt1);
+	System.out.println(txt2);
+	System.out.println(txt3);
+
+  }
+}
