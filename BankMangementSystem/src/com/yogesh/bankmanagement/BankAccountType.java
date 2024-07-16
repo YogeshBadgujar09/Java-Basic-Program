772040b27b4cd4b204f226a242eae057bd627eb6
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-07-16 23:07:41 +0530

    [MODIFIED] : Yogesh - Add Class for Varios Account Types ---- Work incomplete

---
+++ BankMangementSystem/src/com/yogesh/bankmanagement/BankAccountType.java
@@ -0,0 +1,22 @@
+package com.yogesh.bankmanagement;
+import java.util.Map ;
+import  java.util.HashMap ;
+import java.util.Random;
+import java.util.Scanner;
+
+public class BankAccountType {
+
+    Map<String,BankManagementMain> multipleAccountType = new HashMap<>();
+
+    
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+
+}
