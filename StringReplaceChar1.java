0b69847d1e3c609dca19e7e149d1163154112630
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-07-17 23:51:47 +0530

    [ADDED] : String Demo logic for Project Transaction History

---
+++ StringReplaceChar1.java
@@ -0,0 +1,12 @@
+public class StringReplaceChar1{
+
+	  public static void main(String[] args) {
+	
+		String myStr = "Hello";
+    		char[] myArray = myStr.toCharArray();
+    		
+    		for (char i : myArray) {
+      		System.out.println(i);
+  	  	}
+  	}
+}
