f35989ad575cbd81d47fa62855615d1a336b7fd5
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-07-17 23:51:09 +0530

    [ADDED] : String Demo For Project

---
+++ StringReplaceChar.java
@@ -0,0 +1,10 @@
+public class StringReplaceChar{
+
+  		public static void main(String[] args) {
+
+  		String myStr = "Hello";
+    	char[] myArray = myStr.toCharArray();
+
+    	System.out.println(myArray[0]);
+  	}
+}
