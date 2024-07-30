6a21aef18c2825154975d9ceed6f92b94262baef
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-07-30 20:04:30 +0530

    [ADDED] : Yogesh - append function in StringBuilder Class

---
+++ StringBuildetAppendfun.java
@@ -0,0 +1,28 @@
+public class StringBuildetAppendfun {
+
+    public static void main(String[] args) {
+        
+ 
+		StringBuilder sb1 = new StringBuilder("Welcome to StringBuilder "); 
+		System.out.println("Input: " + sb1); 
+
+		// Appending the boolean value 
+		sb1.append(true); 
+		System.out.println("Output: " + sb1); 
+
+		System.out.println(); 
+
+		StringBuilder sb2 = new StringBuilder("We fail- "); 
+		System.out.println("Input: " + sb2); 
+
+		// Appending the boolean value 
+		sb2.append(false); 
+		System.out.println("Output: " + sb2); 
+	} 
+} 
+
+
+
+
+    
+
