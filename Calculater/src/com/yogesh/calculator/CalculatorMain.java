7848269e830277b32b987aadfa4348699525e929
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-08-09 23:17:42 +0530

    [MODIFIED] : Yogesh - Optimise Code for JButton

--- Calculater/src/com/yogesh/calculator/CalculatorMain.java
+++ Calculater/src/com/yogesh/calculator/CalculatorMain.java
@@ -1,22 +1,45 @@
 package com.yogesh.calculator;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class CalculatorMain extends JFrame {
 
+    JPanel jPanel;
+    public void setButtonOptimizeCode(JButton jButton , String btnName, int btnLocationX, int btnLocationY)
+    {
+        final int BUTTON_WIDTH = 50;
+        final int BUTTON_HEIGHT = 25;
+
+        jButton = new JButton(btnName);
+        jButton.setSize(BUTTON_WIDTH,BUTTON_HEIGHT);
+        jButton.setLocation(btnLocationX,btnLocationY);
+        jButton.setLayout(null);
+        jPanel.add(jButton);
+    }
 
     CalculatorMain()
     {
-        JPanel jPanel = new JPanel();
-        JButton btnAC;
-        JButton btnBackSpace ;
-        JButton btnModule;
-        JButton btnDivide,btn7,btn8,btn9,btnMultiplication,btn4,btn5,btn6,btnMinus;
-        JButton btnDoubleZero;
-        JButton btnZero;
-        JButton btnDot  ;
-        JButton btnEqaul;
+        jPanel = new JPanel();
+        JButton btnAC = null;
+        JButton btnBackSpace = null;
+        JButton btnModule = null;
+        JButton btnDivide = null;
+        JButton btn7 = null ;
+        JButton btn8 = null ;
+        JButton btn9 = null ;
+        JButton btnMultiplication = null;
+        JButton btn4 = null ;
+        JButton btn5 = null ;
+        JButton btn6 = null ;
+        JButton btnMinus = null ;
+        JButton btnDoubleZero = null;
+        JButton btnZero = null;
+        JButton btnDot = null ;
+        JButton btnEqaul = null;
+        JButton btn1 = null;
+        JButton btn2 = null;
+        JButton btn3 = null;
+        JButton btnAdd = null ;
 
         jPanel.setLayout(null);
         //FrameMain
@@ -27,105 +50,35 @@ public class CalculatorMain extends JFrame {
         add(jPanel);
 
         //Button Layer 1
-        btnAC = new JButton("AC");
-        btnAC.setSize(50,25);
-        btnAC.setLocation(10,100);
-        btnAC.setLayout(null);
-        jPanel.add(btnAC);
-
-        btnBackSpace = new JButton("<--");
-        btnBackSpace.setSize(50,25);
-        btnBackSpace.setLocation(70,100);
-        btnBackSpace.setLayout(null);
-        jPanel.add(btnBackSpace);
-
-        btnModule = new JButton("%");
-        btnModule.setSize(50,25);
-        btnModule.setLocation(130,100);
-        btnModule.setLayout(null);
-        jPanel.add(btnModule);
-
-        btnDivide = new JButton("/");
-        btnDivide.setSize(50,25);
-        btnDivide.setLocation(190,100);
-        btnDivide.setLayout(null);
-        jPanel.add(btnDivide);
+        setButtonOptimizeCode(btnAC,"AC",10,100);
+        setButtonOptimizeCode(btnBackSpace,"<--",70,100);
+        setButtonOptimizeCode(btnModule,"%",130,100);
+        setButtonOptimizeCode(btnDivide,"/",190,100);
 
         //Button Layer 2
-        btn7 = new JButton("7");
-        btn7.setSize(50,25);
-        btn7.setLocation(10,135);
-        btn7.setLayout(null);
-        jPanel.add(btn7);
-
-        btn8 = new JButton("8");
-        btn8.setSize(50,25);
-        btn8.setLocation(70,135);
-        btn8.setLayout(null);
-        jPanel.add(btn8);
-
-        btn9 = new JButton("9");
-        btn9.setSize(50,25);
-        btn9.setLocation(130,135);
-        btn9.setLayout(null);
-        jPanel.add(btn9);
-
-        btnMultiplication = new JButton("*");
-        btnMultiplication.setSize(50,25);
-        btnMultiplication.setLocation(190,135);
-        btnMultiplication.setLayout(null);
-        jPanel.add(btnMultiplication);
+        setButtonOptimizeCode(btn7,"7",10,135);
+        setButtonOptimizeCode(btn8,"8",70,135);
+        setButtonOptimizeCode(btn9,"9",130,135);
+        setButtonOptimizeCode(btnMultiplication ,"*",190,135);
 
         //Button Layer 3
-        btn4 = new JButton("4");
-        btn4.setSize(50,25);
-        btn4.setLocation(10,170);
-        btn4.setLayout(null);
-        jPanel.add(btn4);
-
-        btn5 = new JButton("5");
-        btn5.setSize(50,25);
-        btn5.setLocation(70,170);
-        btn5.setLayout(null);
-        jPanel.add(btn5);
-
-        btn6 = new JButton("6");
-        btn6.setSize(50,25);
-        btn6.setLocation(130,170);
-        btn6.setLayout(null);
-        jPanel.add(btn6);
-
-        btnMinus = new JButton("-");
-        btnMinus.setSize(50,25);
-        btnMinus.setLocation(190,170);
-        btnMinus.setLayout(null);
-        jPanel.add(btnMinus);
+        setButtonOptimizeCode(btn4,"4",10,170);
+        setButtonOptimizeCode(btn5,"5",70,170);
+        setButtonOptimizeCode(btn6,"6",130,170);
+        setButtonOptimizeCode(btnMinus ,"-",190,170);
 
         //Button Layer 4
+        setButtonOptimizeCode(btn1,"1",10,170);
+        setButtonOptimizeCode(btn2,"2",70,170);
+        setButtonOptimizeCode(btn3,"3",130,170);
+        setButtonOptimizeCode(btnAdd ,"+",190,170);
 
-        btnDoubleZero = new JButton("00");
-        btnDoubleZero.setSize(50,25);
-        btnDoubleZero.setLocation(10,205);
-        btnDoubleZero.setLayout(null);
-        jPanel.add(btnDoubleZero);
+        //Button Layer 5
+        setButtonOptimizeCode(btnDoubleZero,"00",10,205);
+        setButtonOptimizeCode(btnZero,"0",70,205);
+        setButtonOptimizeCode(btnDot,".",130,205);
+        setButtonOptimizeCode(btnEqaul ,"=",190,205);
 
-        btnZero = new JButton("0");
-        btnZero.setSize(50,25);
-        btnZero.setLocation(70,205);
-        btnZero.setLayout(null);
-        jPanel.add(btnZero);
-
-        btnDot = new JButton(".");
-        btnDot.setSize(50,25);
-        btnDot.setLocation(130,205);
-        btnDot.setLayout(null);
-        jPanel.add(btnDot);
-
-        btnEqaul = new JButton("=");
-        btnEqaul.setSize(50,25);
-        btnEqaul.setLocation(190,205);
-        btnEqaul.setLayout(null);
-        jPanel.add(btnEqaul);
 
 
 
