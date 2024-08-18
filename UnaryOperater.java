fa4ec35ed4fa677b1bee390ef0103caf76ae3dcc
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-08-18 23:22:26 +0530

    [ADDED] : Yogesh - Unary Basic

---
+++ UnaryOperater.java
@@ -0,0 +1,12 @@
+public class UnaryOperater {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println(i); // Output: 5
+        System.out.println(i++); // Output: 5
+        System.out.println(i); // Output: 6
+        System.out.println(--i); // Output: 5
+        System.out.println(i); // Output: 5
+        System.out.println(i++ + ++i); // Output: 11
+        System.out.println(i); // Output: 7
+    }
+}
