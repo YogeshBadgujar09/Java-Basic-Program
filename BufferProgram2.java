e12eba2f44776fb2bbf901be990e247bf1591894
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-08-24 23:04:31 +0530

    [ADDED] : Yogesh - Demo 2

---
+++ BufferProgram2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BufferProgram2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        // Create a buffer
+        StringBuffer buffer = new StringBuffer(input);
+
+        // Append to the buffer
+        buffer.append(" is a nice string!");
+
+        // Insert into the buffer
+        buffer.insert(0, "This ");
+
+        // Replace in the buffer
+        buffer.replace(5, 10, "great");
+
+        // Reverse the buffer
+        buffer.reverse();
+
+        System.out.println(buffer.toString());
+    }
+}
