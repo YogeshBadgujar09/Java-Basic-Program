757a641328756b4171732e3c4533c4547c3b8189
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-08-27 23:13:22 +0530

    [ADDED] : Yogesh - sqrt() Function use

---
+++ MathSqrtFunction.java
@@ -0,0 +1,13 @@
+public class MathSqrtFunction {
+    public static void main(String[] args) {
+        double number = 16;
+        double result = Math.sqrt(number);
+        System.out.println("Square root of " + number + " is " + result);
+    }
+}
+
+
+ 
+
+
+
