07e1ca0bc4c8d4847db89f70e181f44334e6d65a
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-08-27 23:14:15 +0530

    [ADDED] : Yogesh - sort() Function use

---
+++ ArraySortFunction.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class ArraySortFunction {
+    public static void main(String[] args) {
+        int[] array = {5, 2, 8, 1, 9};
+        Arrays.sort(array);
+        System.out.println("Sorted array: " + Arrays.toString(array));
+    }
+}
