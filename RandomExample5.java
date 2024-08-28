dba8b67b72c548ea6d9ec741008db97c7c9f6da3
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-08-28 23:45:27 +0530

    [ADDED] : Yogesh - Demo

---
+++ RandomExample5.java
@@ -0,0 +1,7 @@
+public class RandomExample5 {
+    public static void main(String[] args) {
+        // Generate a random double between 0.0 and 1.0
+        double randomValue = Math.random();
+        System.out.println("Random value between 0.0 and 1.0: " + randomValue);
+    }
+}
