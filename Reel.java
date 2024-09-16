2dfe035f8db13eca00efd675e63c8b1d01990f66
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-09-16 20:27:31 +0530

    [ADDED] : Yogesh - reel program

---
+++ Reel.java
@@ -0,0 +1,13 @@
+/**
+ * Reel
+ */
+public class Reel {
+
+     public static void main(String[] args) {
+        int a=10;
+
+        int b = ++a + a++ + --a;
+
+        System.out.println("b=" + b);
+     }
+}
