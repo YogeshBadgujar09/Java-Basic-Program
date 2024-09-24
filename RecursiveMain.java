8877463016254de36101037335f832bfc5f71d37
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-09-24 21:52:06 +0530

    [ADDED] : Yogesh - Recursive main function

---
+++ RecursiveMain.java
@@ -0,0 +1,6 @@
+public class RecursiveMain {
+    public static void main(String[] args) {
+        System.out.println("Main function called");
+        main(args); // Call main function again
+    }
+}
