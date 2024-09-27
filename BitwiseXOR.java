48ed67fb3797236dc0c84d6b0a1480f80a391d3c
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-09-27 21:09:39 +0530

    [ADDED] : Yogesh - Bitwise Revision

---
+++ BitwiseXOR.java
@@ -0,0 +1,10 @@
+public class BitwiseXOR {
+    public static void main(String[] args) {
+        int num1 = 10;  // 1010 in binary
+        int num2 = 12;  // 1100 in binary
+
+        int result = num1 ^ num2;
+
+        System.out.println("The result of num1 ^ num2 is: " + result);
+    }
+}
