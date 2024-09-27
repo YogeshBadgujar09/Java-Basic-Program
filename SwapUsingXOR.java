a635edccf2588b943599b79bb6e3eb8b735e286a
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-09-27 21:10:49 +0530

    [ADDED] : Yogesh - Swaping using XOR

---
+++ SwapUsingXOR.java
@@ -0,0 +1,19 @@
+public class SwapUsingXOR {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        System.out.println("Before swapping:");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Swap num1 and num2 using XOR
+        num1 = num1 ^ num2;
+        num2 = num1 ^ num2;
+        num1 = num1 ^ num2;
+
+        System.out.println("After swapping:");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+    }
+}
