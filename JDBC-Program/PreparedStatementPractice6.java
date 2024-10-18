9f14619311e63ea0e26aad9fbb21a738029030a2
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-10-18 22:47:05 +0530

    [ADDED] : Yogesh - Demo 6

--- JDBC-Program/PreparedStatementPractice6.java
+++ JDBC-Program/PreparedStatementPractice6.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.Scanner;
 
-class PreparedStatementPractice5
+class PreparedStatementPractice6
 {
 
 	public static void main(String args[])
