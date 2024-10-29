6f7d4f47e4ed886a0aff608dd561cce3787712b2
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-10-29 20:20:53 +0530

    [ADDED] : Yogesh - Current Date andTime

---
+++ CurrentDateTime.java
@@ -0,0 +1,14 @@
+//Program : Current Date and Time
+
+
+import java.time.LocalDateTime;
+
+public class CurrentDateTime {
+    public static void main(String[] args) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        System.out.println("Current Date and Time: " + currentDateTime);
+    }
+}
+
+
+ 
