b3b0fc5398ba1a54e317674b96ec689ff26b5784
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-10-30 21:34:58 +0530

    [MODIFIED] : Yogesh - SignUp function work but with One Problem Unique Constraint Violate

--- SignINandUPDemo/src/com/yogesh/credential/CredentialMain.java
+++ SignINandUPDemo/src/com/yogesh/credential/CredentialMain.java
@@ -1,32 +1,71 @@
 package com.yogesh.credential;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
+import oracle.jdbc.driver.OracleDriver;
+
+
 public class CredentialMain {
-	
-	private int userId = 0;
-	private String userName = null ;
-	
+	 
 	public void signUp()
 	{
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter User Id :");
-		this.userId = scanner.nextInt();
+		int userId = 0;
+		String userName = null ;
 		
-		System.out.println("Enter Username :");
-		this.userName = scanner.next();
+		Scanner scanner = new Scanner(System.in);
+		try {
+			
+			//Step 1 : Register Driver  
+			Driver driver = new OracleDriver();
+			DriverManager.registerDriver(driver);
+			
+			//Step 2 : Get Connection 
+			Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521","system","2345");
+			
+			//Step 3 : Statement object 
+			PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO InfoTable VALUES(?,?)");
+			
+			//Step 4 : Execute Query
+			System.out.println("Enter User Id :");
+			userId = scanner.nextInt();
+			preparedStatement.setInt(1, userId);
+			
+			System.out.println("Enter Username :");
+			userName = scanner.next();
+			preparedStatement.setString(2, userName );
+			
+			preparedStatement.executeUpdate(); 
+			
+			//Step 5 : Connection Close
+			connection.close();
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Problem to connection");
+			e.printStackTrace();
+		}
+		
+		
+			
+		
 	}
 	
-	public void getDetails()
-	{
-		System.out.println("\nUser Id :" + this.userId + "\t Username :" + this.userName);
-	} 
+//	public void getDetails()
+//	{
+//		System.out.println("\nUser Id :" + this.userId + "\t Username :" + this.userName);
+//	} 
 
 	public static void main(String[] args) {
 		 
 		CredentialMain cm = new CredentialMain();
 		cm.signUp();
-		cm.getDetails();
+		//cm.getDetails();
 	}
 
 }
