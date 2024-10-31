02e056fa7c6500f69f01e35169386198e7bf9e09
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-10-31 23:43:41 +0530

    [ADDED] : Yogesh - demo 1

---
+++ CurrentTimeDemo1.java
@@ -0,0 +1,14 @@
+
+//Program  : Current Time
+
+
+import java.time.LocalTime;
+
+public class CurrentTimeDemo1 {
+    public static void main(String[] args) {
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("Current Time: " + currentTime);
+    }
+}
+
+
