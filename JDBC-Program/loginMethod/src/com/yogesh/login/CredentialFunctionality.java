1ec0521d7c2eefb06564cbc41d0189c25c5a412a
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-11-19 13:51:48 +0530

    [MODIFIED] : Yogesh - Add SignIn functionality

--- JDBC-Program/loginMethod/src/com/yogesh/login/CredentialFunctionality.java
+++ JDBC-Program/loginMethod/src/com/yogesh/login/CredentialFunctionality.java
@@ -8,7 +8,7 @@ public class CredentialFunctionality {
 
     CredentialFunctionality(){
 
-        signUp();
+        signIn();
 
     }
 
@@ -36,7 +36,7 @@ public class CredentialFunctionality {
                 System.out.print("Password : ");
                 String password = scanner.next();
 
-                ResultSet resultSetUsernameAlreadyAvl = statement.executeQuery("SELECT username FROM credential WHERE username = '"+ userName +"'");
+                ResultSet resultSetUsernameAlreadyAvl = statement.executeQuery("SELECT * FROM credential WHERE username = '"+ userName +"'");
 
                 if(resultSetUsernameAlreadyAvl.next()) {
                     System.out.println("Username already exist , Please enter another Username .... !!! ");
@@ -58,6 +58,55 @@ public class CredentialFunctionality {
             throw new RuntimeException(e);
         }
 
+    }
+
+    public void signIn()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            Driver driver = new oracle.jdbc.driver.OracleDriver();
+            DriverManager.registerDriver(driver);
+
+            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521","system","2345");
+
+            Statement statement = connection.createStatement();
+
+            boolean flag = false;
+
+            do {
+
+                System.out.print("UserName : ");
+                String userName = scanner.next();
+
+                System.out.print("Password : ");
+                String password = scanner.next();
+
+                ResultSet resultSetUsernameAlreadyAvl = statement.executeQuery("SELECT * FROM credential WHERE username = '"+ userName +"'");
+
+                if(resultSetUsernameAlreadyAvl.next()) {
+
+                    if(resultSetUsernameAlreadyAvl.getString(1).equals(userName) && resultSetUsernameAlreadyAvl.getString(2).equals(password))
+                    {
+                        System.out.println("Welcome Back .... !!! ");
+                        flag = true ;
+                    } else if(resultSetUsernameAlreadyAvl.getString(1).equals(userName)){
+                        System.out.println("Please enter valid Password ... !!!");
+                    }
+
+                }
+                else {
+                    System.out.println("Please enter valid Username OR Password .... !!! ");
+                }
+
+            }while(!flag);
+
+            connection.close();
+
+        } catch (SQLException e) {
+            System.out.println("System Message :" + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
 
