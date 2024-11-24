0abe441719a37da22f9e181ef59179958afaaac4
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-11-24 23:01:38 +0530

    [ADDED] : Yogesh - NetBean first Project on working

---
+++ CredentialProcess/src/credentialprocess/CredentialProcess.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package credentialprocess;
+
+import com.yogesh.frontend.VarificationPage;
+/**
+ *
+ * @author Yogesh
+ */
+public class CredentialProcess {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VarificationPage vari = new VarificationPage();
+     
+    }
+    
+}
