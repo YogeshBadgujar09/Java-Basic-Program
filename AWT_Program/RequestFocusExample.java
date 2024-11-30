dc183cc28cae977e4a8f4003a7b66c72b87ca42b
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-11-30 22:44:31 +0530

    [MODIFIED] : Yogesh - focus on specific component

---
+++ AWT_Program/RequestFocusExample.java
@@ -0,0 +1,23 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RequestFocusExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Request Focus Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        JTextField textField1 = new JTextField(10);
+        JTextField textField2 = new JTextField(10);
+
+        frame.add(textField1);
+        frame.add(textField2);
+
+        // Request focus for textField2
+        textField2.requestFocus();
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
