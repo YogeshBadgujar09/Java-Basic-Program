4ed8c1896878f94464a0a22649c3caf0d3b5b209
YogeshBadgujar09 <yogeshsb18@gmail.com>
2025-01-29 22:56:23 +0530

    [ADDED] : Yogesh - Marquee Text Demo 2

---
+++ MarqueeTextDemo2.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MarqueeTextDemo2 extends JPanel {
+
+    private String text = "This is a marquee text example";
+    private int x = 800;
+
+    public MarqueeTextDemo() {
+        setPreferredSize(new Dimension(800, 500));
+        setBackground(Color.BLACK);
+        setForeground(Color.WHITE);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setFont(new Font("Arial", Font.BOLD, 24));
+        g.drawString(text, x, 250);
+    }
+
+    public void animate() {
+        x--;
+        if (x < -getTextWidth(text)) {
+            x = 800;
+        }
+        repaint();
+    }
+
+    private int getTextWidth(String text) {
+        FontMetrics fm = getFontMetrics(new Font("Arial", Font.BOLD, 24));
+        return fm.stringWidth(text);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Marquee Text");
+        MarqueeTextDemo2 marqueeText = new MarqueeTextDemo2();
+
+        frame.add(marqueeText);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        Timer timer = new Timer(1, e -> marqueeText.animate());
+        timer.start();
+    }
+}
