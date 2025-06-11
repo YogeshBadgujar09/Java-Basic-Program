96b7f2384d3b17f7fa718976813c7b550db5102e
YogeshBadgujar09 <yogeshsb18@gmail.com>
2025-06-11 14:20:45 +0530

    [ADDED] : Yogesh - Revised Logic

---
+++ PrimeCheck.java
@@ -0,0 +1,23 @@
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int number = 29;
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            // check from 2 to number-1
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            System.out.println(number + " is a Prime Number.");
+        else
+            System.out.println(number + " is not a Prime Number.");
+    }
+}
