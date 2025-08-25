ef18bff6a24db56cc5aa9bac118c183ddcc5aa6c
YogeshBadgujar09 <yogeshsb18@gmail.com>
2025-08-25 19:42:00 +0530

    [YOGESH] : Yogesh - Question 1 Error Study

---
+++ Question1.java
@@ -0,0 +1,25 @@
+//Q.1 Consider the program given below. What will be the out put if the above program is Executed.
+
+package com.shop.retail;
+package com.book.chap1;
+
+class Question1{
+
+	public static void main(String args[])
+	{
+		if(1){
+			System.out.println("Hello");
+		}else{
+			System.out.println("World");
+		}
+	}
+}
+
+/*
+****** OUTPUT ********
+Question.java : 2 : error : class, interface or enum expected 
+package com.book.chap1
+^
+error
+
+*/
